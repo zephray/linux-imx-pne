76d17edc1a571954e69544c38bdbed54d82dbf2f
Wenting Zhang <zephray@outlook.com>
2018-09-07 18:29:52 -0400

    Migrate to ULL

--- generate_image.sh
+++ generate_image.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cp arch/arm/boot/zImage zImage-with-dtb.bin
-cat arch/arm/boot/dts/imx6ul-14x14-pne.dtb >> zImage-with-dtb.bin
+cat arch/arm/boot/dts/imx6ull-14x14-pne.dtb >> zImage-with-dtb.bin
 ../uboot/tools/mkimage -n imximage.cfg -T imximage -e 0x80800000 -d zImage-with-dtb.bin zImage-with-dtb.imx
--- make_kernel.sh
+++ make_kernel.sh
@@ -1,2 +1,6 @@
-#!/bin/bash
-ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- time make -j4
+#!/bin/sh
+export CPUS=`grep -c processor /proc/cpuinfo`
+export ARCH=arm
+export CROSS_COMPILE=/opt/gcc-linaro-5.5.0-2017.10-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-
+export PATH=/opt/gcc-linaro-5.5.0-2017.10-x86_64_arm-linux-gnueabihf/bin:$PATH
+time make -j${CPUS}
